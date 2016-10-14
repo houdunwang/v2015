cb62c7431f5ebdd90e88babe9d986663f66a6f4a
向军 <2300071698@qq.com>
2016-10-14 14:42:37 +0800

    hdphp 3.0 第7课

--- hdphp3/app/home/controller/Entry.php
+++ hdphp3/app/home/controller/Entry.php
@@ -2,11 +2,21 @@
 
 class Entry {
 	public function index() {
-//		dd(u('admin/user/add',['id'=>1,'cid'=>22]));
+		//		dd(u('admin/user/add',['id'=>1,'cid'=>22]));
 		return view();
 	}
 
-	public function add(){
+	public function add() {
 		echo 'add...';
+	}
+
+	public function change() {
+		//		View::with( 'a', 23 );
+		View::with( "data", [ 'username' => '后盾网', "url" => 'houdunwang.com' ] );
+		View::with( "news", [ 'title' => '后盾人在线学习平台' ] );
+
+//		return view()->with( 'url', 'houdunren.com' );
+//		return View::with('url','www.houdunren.com')->make();
+		return View::make()->with('url','www.houdunren.com');
 	}
 }

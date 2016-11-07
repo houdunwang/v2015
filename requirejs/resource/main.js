e21e721975dd103c192f530ee9a3e874c1a011cf
向军 <2300071698@qq.com>
2016-11-07 22:59:59 +0800

    require.js

---
+++ requirejs/resource/main.js
@@ -0,0 +1,10 @@
+require.config({
+    baseUrl: '../resource/lib',
+    paths: {
+        'jquery': '../js/jquery.min',
+        'angular': 'angular.min'
+    }
+});
+// require(['jquery', 'angular'], function ($, angular) {
+//     $('body').css({'backgroundColor': 'red'});
+// })
